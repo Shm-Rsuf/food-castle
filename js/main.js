e8c7b7505159c4cb80db95696b070bd9ba1f7e4f
shm_usuf <shmusuf@gmail.com>
2022-09-27 09:08:15 +0600

    project done

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/*MOBILE NAV OR HIDE*/
+
+const navButton = document.querySelector('.btn-mobile-nav');
+const header = document.querySelector('.header');
+
+navButton.addEventListener('click', () => {
+  header.classList.toggle('nav-open');
+})
+
+/*NAV-BAR STICKY EVENTS*/
+
+const sectionHero = document.querySelector('.section-hero');
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(sectionHero);
